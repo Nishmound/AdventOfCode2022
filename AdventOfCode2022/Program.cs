d12a5b743163f129f29276997a3eacf817f36096
Nishmound <dominikide@outlook.de>
2023-08-29 12:00:27 +0200

    Day 1 completed

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using AdventOfCode2022;
+
+Console.WriteLine("Advent of Code\n$year=2022;");
+write_empty();
+
+#region Day1
+write_empty();
+write_sep();
+Console.WriteLine("--- Day 1: Calorie Counting ---");
+write_empty();
+int day1_result;
+using (var reader = new StreamReader(@"inputs\adventofcode.com_2022_day_1_input.txt")) 
+    day1_result = Day1.run(reader);
+Console.WriteLine($"The Elf that carries the most calories carries:\n{day1_result}");
+#endregion
+
+
+Console.ReadLine();
+
+static void write_sep() => Console.WriteLine(string.Join("", Enumerable.Repeat("#", 50)));
+static void write_empty() => Console.WriteLine();
